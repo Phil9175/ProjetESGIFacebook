6aa47ff58c30fa48bf900e2bb4bd306fac3fc70a
Philippe Pierre Granger <philgranger@orange.fr>
2015-12-07 11:02:13 +0100

    modify sql

--- schema.sql
+++ schema.sql
@@ -1,42 +1,104 @@
-DROP SCHEMA IF EXISTS "concoursPhotos" CASCADE;
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost:3306
+-- Généré le :  Lun 07 Décembre 2015 à 10:50
+-- Version du serveur :  5.5.42-log
+-- Version de PHP :  5.6.10
 
-CREATE SCHEMA "concoursPhotos";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-CREATE TABLE "concoursPhotos"."concours"
-(
-  "id_Concours" int NOT NULL,
-  "name" VARCHAR  NOT NULL,
-  "description" VARCHAR,
-  "award" VARCHAR,
-  "start_date" DATETIME_INTERVAL_CODE  NOT NULL,
-  "end_date" DATETIME_INTERVAL_CODE  NOT NULL,
-  "status" bool,
-  "ranking" text,
-  PRIMARY KEY ("idConcours")
-);
+--
+-- Base de données :  `concoursphotos`
+--
 
-CREATE TABLE "concoursPhotos"."participation"
-(
-  "id_competition" int NOT NULL,
-  "id_participant" int NOT NULL,
-  "id_photo"int NOT NULL,
-  FOREIGN KEY ("id_competition")
-      REFERENCES "concoursPhotos"."concours" ("id_competition") MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  FOREIGN KEY ("id_participant")
-      REFERENCES "concoursPhotos"."participant" ("id_participant") MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-);
+-- --------------------------------------------------------
 
-CREATE TABLE "concoursPhotos"."participant"
-(
-  "id" int NOT NULL,
-  "name" VARCHAR NOT NULL,
-  "firstName" VARCHAR NOT NULL,
-  "gender" VARCHAR,
-  "email" VARCHAR NOT NULL,
-  "birth_date" DATE ,
-  "city" VARCHAR,
-  "role" VARCHAR,
-  PRIMARY KEY ("id_participant")
-);
+--
+-- Structure de la table `competition`
+--
+
+CREATE TABLE `competition` (
+  `id_competition` int(11) NOT NULL,
+  `name` varchar(75) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `award` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `status` varchar(25) NOT NULL,
+  `ranking` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `participant`
+--
+
+CREATE TABLE `participant` (
+  `id` int(11) NOT NULL,
+  `name` varchar(75) NOT NULL,
+  `firstname` varchar(75) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `birthdate` date NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `participation`
+--
+
+CREATE TABLE `participation` (
+  `id` int(11) NOT NULL,
+  `id_competition` int(11) NOT NULL,
+  `id_participation` int(11) NOT NULL,
+  `id_photo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `competition`
+--
+ALTER TABLE `competition`
+  ADD PRIMARY KEY (`id_competition`);
+
+--
+-- Index pour la table `participant`
+--
+ALTER TABLE `participant`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `participation`
+--
+ALTER TABLE `participation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `competition`
+--
+ALTER TABLE `competition`
+  MODIFY `id_competition` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `participant`
+--
+ALTER TABLE `participant`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `participation`
+--
+ALTER TABLE `participation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
