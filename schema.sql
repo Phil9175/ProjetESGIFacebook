8801cd29d35f00299f2137d60750fb2758c801b4
Philippe Pierre Granger <philgranger@orange.fr>
2016-02-14 18:51:38 +0100

    sql

--- schema.sql
+++ schema.sql
@@ -1,17 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:3306
--- Généré le :  Lun 07 Décembre 2015 à 10:50
--- Version du serveur :  5.5.42-log
--- Version de PHP :  5.6.10
+-- Généré le :  Dim 14 Février 2016 à 18:41
+-- Version du serveur :  5.1.73
+-- Version de PHP :  5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
--- Base de données :  `concoursphotos`
+-- Base de données :  `concoursphotosesgi`
 --
 
 -- --------------------------------------------------------
@@ -21,52 +27,132 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `concours` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(75) NOT NULL,
   `description` text NOT NULL,
-  `award` text NULL,
-  `start_date` DATETIME NOT NULL,
-  `end_date` DATETIME NOT NULL,
-  `status` boolean NOT NULL,
-  `ranking` text NULL,
-  `logo` varchar(255) NULL,
-  `font` varchar(255) NULL,
-  `font_family` varchar(255) NULL,
-  `font_color` varchar(24) NULL,
-  `background_color` varchar(24) NULL,
-  `max_per_page` int(255) NULL,
-  PRIMARY KEY(id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
+  `award` text,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `ranking` text,
+  `logo` varchar(255) DEFAULT NULL,
+  `font` varchar(255) DEFAULT NULL,
+  `font_family` varchar(255) DEFAULT NULL,
+  `font_color` varchar(24) DEFAULT NULL,
+  `background_color` varchar(24) DEFAULT NULL,
+  `max_per_page` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Structure de la table `participant`
 --
 
 CREATE TABLE `participant` (
-  `id_participant` bigint NOT NULL,
+  `id_participant` bigint(20) NOT NULL,
   `name` varchar(75) NOT NULL,
   `first_name` varchar(75) NOT NULL,
   `last_name` varchar(75) NOT NULL,
-  `gender` boolean NOT NULL,
+  `gender` tinyint(1) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `birthdate` varchar(25) NULL,
-  `city` varchar(50) NULL,
-  `role` varchar(20) NOT NULL,
-  PRIMARY KEY(id_participant)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
+  `birthdate` varchar(25) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Structure de la table `participation`
 --
 
 CREATE TABLE `participation` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL,
   `id_concours` int(11) NOT NULL,
-  `id_participant` bigint NOT NULL,
-  `id_photo` bigint NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
+  `id_participant` bigint(20) NOT NULL,
+  `id_photo` bigint(20) NOT NULL,
+  `id_photo_name` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-  PRIMARY KEY(id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `settings`
+--
 
-ALTER TABLE participation add foreign key (id_concours) References concours(id);
-ALTER TABLE participation add foreign key (id_participant) References participant(id_participant);
+CREATE TABLE `settings` (
+  `id` int(11) NOT NULL,
+  `logo_concours` text NOT NULL,
+  `nom_societe` varchar(60) NOT NULL,
+  `mail_host` varchar(80) NOT NULL,
+  `mail_port` int(11) NOT NULL,
+  `mail_username` varchar(80) NOT NULL,
+  `mail_password` varchar(80) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `concours`
+--
+ALTER TABLE `concours`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `participant`
+--
+ALTER TABLE `participant`
+  ADD PRIMARY KEY (`id_participant`);
+
+--
+-- Index pour la table `participation`
+--
+ALTER TABLE `participation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_concours` (`id_concours`),
+  ADD KEY `id_participant` (`id_participant`);
+
+--
+-- Index pour la table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `concours`
+--
+ALTER TABLE `concours`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `participation`
+--
+ALTER TABLE `participation`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `participation`
+--
+ALTER TABLE `participation`
+  ADD CONSTRAINT `participation_ibfk_1` FOREIGN KEY (`id_concours`) REFERENCES `concours` (`id`),
+  ADD CONSTRAINT `participation_ibfk_2` FOREIGN KEY (`id_participant`) REFERENCES `participant` (`id_participant`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
