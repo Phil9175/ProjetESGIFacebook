6cd39238e9a36ce3249907749525ebdf66d20259
Philippe Pierre Granger <philgranger@orange.fr>
2016-02-14 18:59:17 +0100

    Admin Interface settings form

--- view/layout/admin2.layout.php
+++ view/layout/admin2.layout.php
@@ -113,7 +113,7 @@ License: You must have a valid license purchased only from themeforest(the above
 			<!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
 			<!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
 			<!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
-			<ul class="page-sidebar-menu page-sidebar-menu-closed" data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
+			<ul class="page-sidebar-menu" data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
 				<li class="start ">
 					<a href="index.html">
 					<i class="icon-home"></i>
@@ -720,28 +720,7 @@ License: You must have a valid license purchased only from themeforest(the above
 	<!-- BEGIN CONTENT -->
 	<div class="page-content-wrapper">
 		<div class="page-content">
-			<!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->
-			<div class="modal fade" id="portlet-config" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-							<h4 class="modal-title">Modal title</h4>
-						</div>
-						<div class="modal-body">
-							 Widget settings form goes here
-						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn blue">Save changes</button>
-							<button type="button" class="btn default" data-dismiss="modal">Close</button>
-						</div>
-					</div>
-					<!-- /.modal-content -->
-				</div>
-				<!-- /.modal-dialog -->
-			</div>
-			<!-- /.modal -->
-			<!-- END SAMPLE PORTLET CONFIGURATION MODAL FORM-->
+			
 			<!-- BEGIN PAGE HEADER-->
 			<!-- BEGIN PAGE HEAD -->
 			<div class="page-head">
@@ -750,95 +729,6 @@ License: You must have a valid license purchased only from themeforest(the above
 					<h1>User Account <small>user account page sample</small></h1>
 				</div>
 				<!-- END PAGE TITLE -->
-				<!-- BEGIN PAGE TOOLBAR -->
-				<div class="page-toolbar">
-					<!-- BEGIN THEME PANEL -->
-					<div class="btn-group btn-theme-panel">
-						<a href="javascript:;" class="btn dropdown-toggle" data-toggle="dropdown">
-						<i class="icon-settings"></i>
-						</a>
-						<div class="dropdown-menu theme-panel pull-right dropdown-custom hold-on-click">
-							<div class="row">
-								<div class="col-md-4 col-sm-4 col-xs-12">
-									<h3>THEME</h3>
-									<ul class="theme-colors">
-										<li class="theme-color theme-color-default active" data-theme="default">
-											<span class="theme-color-view"></span>
-											<span class="theme-color-name">Dark Header</span>
-										</li>
-										<li class="theme-color theme-color-light" data-theme="light">
-											<span class="theme-color-view"></span>
-											<span class="theme-color-name">Light Header</span>
-										</li>
-									</ul>
-								</div>
-								<div class="col-md-8 col-sm-8 col-xs-12 seperator">
-									<h3>LAYOUT</h3>
-									<ul class="theme-settings">
-										<li>
-											 Theme Style
-											<select class="layout-style-option form-control input-small input-sm">
-												<option value="square" selected="selected">Square corners</option>
-												<option value="rounded">Rounded corners</option>
-											</select>
-										</li>
-										<li>
-											 Layout
-											<select class="layout-option form-control input-small input-sm">
-												<option value="fluid" selected="selected">Fluid</option>
-												<option value="boxed">Boxed</option>
-											</select>
-										</li>
-										<li>
-											 Header
-											<select class="page-header-option form-control input-small input-sm">
-												<option value="fixed" selected="selected">Fixed</option>
-												<option value="default">Default</option>
-											</select>
-										</li>
-										<li>
-											 Top Dropdowns
-											<select class="page-header-top-dropdown-style-option form-control input-small input-sm">
-												<option value="light">Light</option>
-												<option value="dark" selected="selected">Dark</option>
-											</select>
-										</li>
-										<li>
-											 Sidebar Mode
-											<select class="sidebar-option form-control input-small input-sm">
-												<option value="fixed">Fixed</option>
-												<option value="default" selected="selected">Default</option>
-											</select>
-										</li>
-										<li>
-											 Sidebar Menu
-											<select class="sidebar-menu-option form-control input-small input-sm">
-												<option value="accordion" selected="selected">Accordion</option>
-												<option value="hover">Hover</option>
-											</select>
-										</li>
-										<li>
-											 Sidebar Position
-											<select class="sidebar-pos-option form-control input-small input-sm">
-												<option value="left" selected="selected">Left</option>
-												<option value="right">Right</option>
-											</select>
-										</li>
-										<li>
-											 Footer
-											<select class="page-footer-option form-control input-small input-sm">
-												<option value="fixed">Fixed</option>
-												<option value="default" selected="selected">Default</option>
-											</select>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!-- END THEME PANEL -->
-				</div>
-				<!-- END PAGE TOOLBAR -->
 			</div>
 			<!-- END PAGE HEAD -->
 			<!-- BEGIN PAGE BREADCRUMB -->
@@ -921,26 +811,6 @@ License: You must have a valid license purchased only from themeforest(the above
 						<div class="row">
 							<div class="col-md-12">
 								<div class="portlet light">
-									<div class="portlet-title tabbable-line">
-										<div class="caption caption-md">
-											<i class="icon-globe theme-font hide"></i>
-											<span class="caption-subject font-blue-madison bold uppercase">Profile Account</span>
-										</div>
-										<ul class="nav nav-tabs">
-											<li class="active">
-												<a href="#tab_1_1" data-toggle="tab">Personal Info</a>
-											</li>
-											<li>
-												<a href="#tab_1_2" data-toggle="tab">Change Avatar</a>
-											</li>
-											<li>
-												<a href="#tab_1_3" data-toggle="tab">Change Password</a>
-											</li>
-											<li>
-												<a href="#tab_1_4" data-toggle="tab">Privacy Settings</a>
-											</li>
-										</ul>
-									</div>
 									<div class="portlet-body">
 										<div class="tab-content">
 											<!-- PERSONAL INFO TAB -->
