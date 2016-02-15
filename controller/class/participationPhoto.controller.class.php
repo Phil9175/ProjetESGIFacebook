b5a4743101f4eb485410851a7d49dacfab13529f
PPG <philgranger@orange.fr>
2016-02-15 23:17:24 +0100

    Adresse sire

--- controller/class/participationPhoto.controller.class.php
+++ controller/class/participationPhoto.controller.class.php
@@ -16,7 +16,7 @@ class participationPhoto{
             $helper = $this->fb->getRedirectLoginHelper();
             $scope =["email","user_likes","user_photos","publish_actions","user_birthday","user_location"];
 
-            $this->loginUrl = $helper->getLoginUrl('http://concoursphotosesgi.localhost/login-callback.php',$scope);
+            $this->loginUrl = $helper->getLoginUrl(ADRESSE_SITE.'login-callback.php',$scope);
         }else{
             $this->fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
         }
