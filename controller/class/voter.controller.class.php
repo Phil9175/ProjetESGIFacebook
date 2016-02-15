1a7a87e0d63ba6739836c82de93f0f134737ae21
PPG <philgranger@orange.fr>
2016-02-15 23:25:27 +0100

    Update

--- controller/class/voter.controller.class.php
+++ controller/class/voter.controller.class.php
@@ -16,7 +16,7 @@ class voter {
 			$helper = $this->fb->getRedirectLoginHelper();
 			$scope =["email","user_likes","user_photos","publish_actions"];
 
-			$this->loginUrl = $helper->getLoginUrl(ADRESSE_SITE.'login-callback.php',$scope);
+			$this->loginUrl = $helper->getLoginUrl(ADRESSE_SITE.'login-callback',$scope);
 		}else{
 			$this->fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
 		}
