eddc61b32ceca12bfcd91e2291293d3b1fe1b3ab
Philippe Pierre Granger <philgranger@orange.fr>
2016-02-16 11:52:44 +0100

    admin callback

--- controller/class/admin.controller.class.php
+++ controller/class/admin.controller.class.php
@@ -17,8 +17,7 @@ class admin
 		if(!isset($_SESSION['facebook_access_token'])){
 			$helper = $this->fb->getRedirectLoginHelper();
 			$scope =["email","user_likes","user_photos","publish_actions","user_birthday","user_location"];
-
-			$this->loginUrl = $helper->getLoginUrl(ADRESSE_SITE.'login-callback.php',$scope);
+			$this->loginUrl = $helper->getLoginUrl(ADRESSE_SITE.'callback/',$scope);
 		}else{
 
 			$this->fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
